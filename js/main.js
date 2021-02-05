e12ab15b2ef3820be5d9b160f83799f612dea177
andreipritula <andreylritula@gmail.com>
2021-02-05 19:28:31 +0200

    first steps in building responsive web page

---
+++ js/main.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+  const flowersSlider = new Swiper('.flowers-slider', {
+    // Параметры слайдера
+    loop: true,
+    slidesPerView: 6,
+  
+    // Вид стрелок
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    //Adaptive version
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 40
+      }
+    }
+  });
+
+  const rewiewsSlider = new Swiper('.reviews-slider', {
+      // Параметры слайдера
+      loop: true,
+      slidesPerView: 1,
+    
+      // Вид стрелок
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      
+  });
+
+  $("#review-1").on('click', function() {
+
+    $.fancybox.open([
+      {
+        src  : 'https://source.unsplash.com/IvfoDk30JnI/1500x1000',
+        opts : {
+          caption : 'First caption',
+          thumb   : 'https://source.unsplash.com/IvfoDk30JnI/240x160'
+        }
+      },
+      {
+        src  : 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
+        opts : {
+          caption : 'Second caption',
+          thumb   : 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
+        }
+      }
+    ], {
+      loop : true,
+      thumbs : {
+        autoStart : true
+      }
+    });
+  
+  });
+
+});
